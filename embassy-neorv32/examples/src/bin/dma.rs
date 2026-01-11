df311a63f58213ec98e02121d8dd0c0c6383322d
Kurtis Dinelle <kurtisdinelle@gmail.com>
2026-01-10 23:45:35 -0800

    Simplify DMA example

--- embassy-neorv32/examples/src/bin/dma.rs
+++ embassy-neorv32/examples/src/bin/dma.rs
@@ -4,65 +4,29 @@
 use embassy_neorv32::bind_interrupts;
 use embassy_neorv32::dma::{self, Dma};
 use embassy_neorv32::peripherals;
-use embassy_neorv32::uart::{self, UartTx};
+use embassy_neorv32::uart::UartTx;
 use embassy_neorv32_examples::*;
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::mutex::Mutex;
-use embassy_sync::once_lock::OnceLock;
-use embassy_time::Timer;
 
 bind_interrupts!(struct Irqs {
     DMA => dma::InterruptHandler<peripherals::DMA>;
-    UART0 => uart::InterruptHandler<peripherals::UART0>;
 });
 
-type SharedUart = Mutex<CriticalSectionRawMutex, UartTx<'static, uart::Async>>;
-static UART: OnceLock<SharedUart> = OnceLock::new();
-
-#[embassy_executor::task]
-async fn dma_transfer(mut dma: Dma<'static>, uart: &'static SharedUart) {
-    loop {
-        let src = [42u8; 1024];
-        let mut dst = [69u8; 1024];
-
-        uart.lock()
-            .await
-            .write(b"DMA transfer started..\n")
-            .await
-            .unwrap();
-        let res = dma.copy(&src, &mut dst, false).await;
-        {
-            let mut uart = uart.lock().await;
-            if res.is_ok() && src.iter().last() == dst.iter().last() {
-                uart.write(b"DMA transfer complete\n").await.unwrap();
-            } else {
-                uart.write(b"DMA transfer failed\n").await.unwrap();
-            }
-        }
-
-        Timer::after_micros(s_to_us(2)).await;
-    }
-}
-
 #[embassy_executor::main]
-async fn main(spawner: embassy_executor::Spawner) {
+async fn main(_spawner: embassy_executor::Spawner) {
     let p = embassy_neorv32::init();
 
-    // Setup UART just for printing WDT state
-    let uart = UartTx::new_async(p.UART0, UART_BAUD, UART_IS_SIM, false, Irqs)
+    let mut uart = UartTx::new_blocking(p.UART0, UART_BAUD, UART_IS_SIM, false)
         .expect("UART must be supported");
-    let uart = UART.get_or_init(|| Mutex::new(uart));
 
-    // Setup DMA
-    let dma = Dma::new(p.DMA, Irqs).expect("DMA must be supported");
-    spawner.must_spawn(dma_transfer(dma, uart));
+    let mut dma = Dma::new(p.DMA, Irqs).expect("DMA must be supported");
 
-    loop {
-        uart.lock()
-            .await
-            .write(b"Doing some work...\n")
-            .await
-            .unwrap();
-        Timer::after_micros(s_to_us(1)).await;
+    let src = [0xAAu8; 1024];
+    let mut dst = [0xFFu8; 1024];
+
+    let res = dma.copy(&src, &mut dst, false).await;
+    match res {
+        Ok(()) if src == dst => uart.blocking_write(b"DMA transfer succeeded\n"),
+        Err(_) => uart.blocking_write(b"DMA transfer encountered an error\n"),
+        _ => uart.blocking_write(b"DMA transfer failed\n"),
     }
 }
